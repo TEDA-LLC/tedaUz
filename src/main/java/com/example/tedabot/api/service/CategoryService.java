28f561298b40eb71292492219b35df91a631f445
Mahmudnazar Sunnatullayev <mahmuddeveloper07@gmail.com>
2022-11-01 11:42:24 +0500

    Category CRUD

---
+++ src/main/java/com/example/tedabot/api/service/CategoryService.java
@@ -0,0 +1,119 @@
+package com.example.tedabot.api.service;
+
+import com.example.tedabot.api.dto.ApiResponse;
+import com.example.tedabot.model.Category;
+import com.example.tedabot.model.Product;
+import com.example.tedabot.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author * Sunnatullayev Mahmudnazar *  * tedabot *  * 11:17 *
+ */
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public ApiResponse<List<Category>> getAll() {
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty()) {
+            return ApiResponse.<List<Category>>builder().
+                    message("Not Found").
+                    status(204).
+                    success(false).
+                    build();
+        } else {
+            return ApiResponse.<List<Category>>builder().
+                    message("Here").
+                    status(200).
+                    success(true).
+                    data(categories).
+                    build();
+        }
+    }
+
+    public ApiResponse<Category> getOne(Long id) {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isEmpty()) {
+            return ApiResponse.<Category>builder().
+                    message("Not Found").
+                    status(204).
+                    success(false).
+                    build();
+        } else {
+            return ApiResponse.<Category>builder().
+                    message("Here").
+                    status(200).
+                    success(true).
+                    data(optionalCategory.get()).
+                    build();
+        }
+    }
+
+
+    public ApiResponse<?> add(Category category) {
+        if (category.getNameUz().isEmpty() || category.getNameRu().isEmpty()) {
+            return ApiResponse.builder().
+                    message("Name is null").
+                    status(204).
+                    success(false).
+                    build();
+        }
+        categoryRepository.save(category);
+        return ApiResponse.builder().
+                message("Saved").
+                status(200).
+                success(true).
+                build();
+    }
+
+    public ApiResponse<?> edit(Category category, Long id) {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isEmpty()) {
+            return ApiResponse.builder().
+                    message("Category Not Found").
+                    status(204).
+                    success(false).
+                    build();
+        }
+        if (category.getNameUz().isEmpty() || category.getNameRu().isEmpty()) {
+            return ApiResponse.builder().
+                    message("Name is null").
+                    status(204).
+                    success(false).
+                    build();
+        }
+        Category categoryOld = optionalCategory.get();
+        categoryOld.setNameUz(category.getNameUz());
+        categoryOld.setNameRu(category.getNameRu());
+        categoryRepository.save(categoryOld);
+
+        return ApiResponse.builder().
+                message("Edited").
+                status(201).
+                success(true).
+                build();
+    }
+
+    public ApiResponse<?> delete(Long id) {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isEmpty()) {
+            return ApiResponse.builder().
+                    message("Category Not Found").
+                    status(204).
+                    success(false).
+                    build();
+        }
+        categoryRepository.deleteById(id);
+        return ApiResponse.builder().
+                message("Deleted").
+                status(201).
+                success(true).
+                build();
+    }
+}
--- src/main/java/com/example/tedabot/api/service/ProductService.java
+++ src/main/java/com/example/tedabot/api/service/ProductService.java
@@ -5,7 +5,6 @@ import com.example.tedabot.api.dto.ProductDTO;
 import com.example.tedabot.model.Attachment;
 import com.example.tedabot.model.Category;
 import com.example.tedabot.model.Product;
-import com.example.tedabot.repository.AttachmentRepository;
 import com.example.tedabot.repository.CategoryRepository;
 import com.example.tedabot.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,41 +24,6 @@ import java.util.Optional;
 public class ProductService {
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
-    private final AttachmentRepository attachmentRepository;
-
-    @SneakyThrows
-    public ApiResponse<?> save(ProductDTO productDTO) {
-        Optional<Category> categoryOptional = categoryRepository.findById(productDTO.getCategoryId());
-        if (categoryOptional.isEmpty()) {
-            return ApiResponse.builder().
-                    message("Category not found").
-                    status(204).
-                    success(false).
-                    build();
-        }
-
-        MultipartFile photo = productDTO.getPhoto();
-        Attachment attachment = new Attachment();
-        attachment.setBytes(photo.getBytes());
-        attachment.setOriginalName(photo.getOriginalFilename());
-
-        Product product = new Product();
-        product.setCategory(categoryOptional.get());
-        product.setAttachment(attachment);
-        product.setPrice(productDTO.getPrice());
-        product.setNameRu(productDTO.getNameRu());
-        product.setNameUz(productDTO.getNameUz());
-        product.setDescriptionRu(productDTO.getDescriptionRu());
-        product.setDescriptionUz(productDTO.getDescriptionUz());
-
-        productRepository.save(product);
-
-        return ApiResponse.builder().
-                message("Saved").
-                status(200).
-                success(true).
-                build();
-    }
 
     public ApiResponse<List<Product>> getAll() {
         List<Product> products = productRepository.findAll();
@@ -97,6 +61,39 @@ public class ProductService {
         }
     }
 
+    @SneakyThrows
+    public ApiResponse<?> save(ProductDTO productDTO) {
+        Optional<Category> categoryOptional = categoryRepository.findById(productDTO.getCategoryId());
+        if (categoryOptional.isEmpty()) {
+            return ApiResponse.builder().
+                    message("Category not found").
+                    status(204).
+                    success(false).
+                    build();
+        }
+
+        MultipartFile photo = productDTO.getPhoto();
+        Attachment attachment = new Attachment();
+        attachment.setBytes(photo.getBytes());
+        attachment.setOriginalName(photo.getOriginalFilename());
+
+        Product product = new Product();
+        product.setCategory(categoryOptional.get());
+        product.setAttachment(attachment);
+        product.setPrice(productDTO.getPrice());
+        product.setNameRu(productDTO.getNameRu());
+        product.setNameUz(productDTO.getNameUz());
+        product.setDescriptionRu(productDTO.getDescriptionRu());
+        product.setDescriptionUz(productDTO.getDescriptionUz());
+
+        productRepository.save(product);
+
+        return ApiResponse.builder().
+                message("Saved").
+                status(200).
+                success(true).
+                build();
+    }
 
     @SneakyThrows
     public ApiResponse<?> edit(ProductDTO productDTO, Long id) {
