b08b91a443d6d96902aaad47746e5df2c204ae70
Mahmudnazar Sunnatullayev <mahmuddeveloper07@gmail.com>
2022-11-09 12:46:21 +0500

    Api update

--- src/main/java/com/example/tedabot/api/controller/CategoryController.java
+++ src/main/java/com/example/tedabot/api/controller/CategoryController.java
@@ -2,11 +2,13 @@ package com.example.tedabot.api.controller;
 
 import com.example.tedabot.api.dto.ApiResponse;
 import com.example.tedabot.api.service.CategoryService;
+import com.example.tedabot.api.service.UserService;
 import com.example.tedabot.model.Category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -18,33 +20,39 @@ import java.util.List;
 @RequestMapping("/api/category")
 public class CategoryController {
     private final CategoryService categoryService;
+    private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<?> getAll() {
+    public ResponseEntity<?> getAll(HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<List<Category>> response = categoryService.getAll();
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getOne(@PathVariable Long id) {
+    public ResponseEntity<?> getOne(@PathVariable Long id, HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<Category> response = categoryService.getOne(id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @PostMapping
-    public ResponseEntity<?> add(@RequestBody Category category) {
+    public ResponseEntity<?> add(@RequestBody Category category, HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<?> response = categoryService.add(category);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> edit(@RequestBody Category category, @PathVariable Long id) {
+    public ResponseEntity<?> edit(@RequestBody Category category, @PathVariable Long id, HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<?> response = categoryService.edit(category, id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
+    public ResponseEntity<?> delete(@PathVariable Long id, HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<?> response = categoryService.delete(id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
--- src/main/java/com/example/tedabot/api/controller/ProductController.java
+++ src/main/java/com/example/tedabot/api/controller/ProductController.java
@@ -3,12 +3,14 @@ package com.example.tedabot.api.controller;
 import com.example.tedabot.api.service.ProductService;
 import com.example.tedabot.api.dto.ApiResponse;
 import com.example.tedabot.api.dto.ProductDTO;
+import com.example.tedabot.api.service.UserService;
 import com.example.tedabot.model.Product;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -19,33 +21,39 @@ import java.util.List;
 @RequestMapping("/api/product")
 public class ProductController {
     private final ProductService productService;
+    private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<?> getAll() {
+    public ResponseEntity<?> getAll(HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<List<Product>> response = productService.getAll();
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getOne(@PathVariable Long id) {
+    public ResponseEntity<?> getOne(@PathVariable Long id, HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<Product> response = productService.getOne(id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @PostMapping
-    public ResponseEntity<?> add(@ModelAttribute ProductDTO productDTO) {
+    public ResponseEntity<?> add(@ModelAttribute ProductDTO productDTO, HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<?> response = productService.save(productDTO);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> edit(@ModelAttribute ProductDTO productDTO, @PathVariable Long id) {
+    public ResponseEntity<?> edit(@ModelAttribute ProductDTO productDTO, @PathVariable Long id, HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<?> response = productService.edit(productDTO, id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
+    public ResponseEntity<?> delete(@PathVariable Long id, HttpServletRequest request) {
+        userService.validateToken(request);
         ApiResponse<?> response = productService.delete(id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
