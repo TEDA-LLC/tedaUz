8cbf7a1d935c6c706c3bc20f0f6e041ff56219e0
umidjon-2231 <tumidjon808@gmail.com>
2022-11-14 19:09:30 +0500

    Update mvn-runner.bat

--- mvn-runner.bat
+++ mvn-runner.bat
@@ -3,4 +3,4 @@ for /f %%i IN ('"mvn help:evaluate -Dexpression=project.name -q -DforceStdout"')
 for /f %%i IN ('"mvn help:evaluate -Dexpression=project.version -q -DforceStdout"') do set project_version=%%i
 @echo building %project_name%-%project_version%
 title %project_name%-%project_version%
-mvn clean install & java -jar target/%project_name%-%project_version%.jar
+mvn clean install & cls & java -jar target/%project_name%-%project_version%.jar
