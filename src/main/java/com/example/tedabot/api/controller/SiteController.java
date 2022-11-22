dac7c7456fa7763f345fb2a59f7275ccb6cd5d7d
Mahmudnazar Sunnatullayev <mahmuddeveloper07@gmail.com>
2022-11-22 18:23:12 +0500

    site controller

---
+++ src/main/java/com/example/tedabot/api/controller/SiteController.java
@@ -0,0 +1,16 @@
+package com.example.tedabot.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author * Sunnatullayev Mahmudnazar *  * tedabot *  * 18:21 *
+ */
+
+@RestController
+@RequestMapping("/api/site")
+@RequiredArgsConstructor
+public class SiteController {
+
+}
