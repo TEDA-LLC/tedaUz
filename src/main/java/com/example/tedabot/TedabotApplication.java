a6eee9f62c97e198c6e108a94e2a11792e09760f
Mahmudnazar Sunnatullayev <mahmuddeveloper07@gmail.com>
2022-11-28 17:11:00 +0500

    set timezone

--- src/main/java/com/example/tedabot/TedabotApplication.java
+++ src/main/java/com/example/tedabot/TedabotApplication.java
@@ -3,10 +3,12 @@ package com.example.tedabot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TedabotApplication {
-
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tashkent"));
         SpringApplication.run(TedabotApplication.class, args);
     }
 
