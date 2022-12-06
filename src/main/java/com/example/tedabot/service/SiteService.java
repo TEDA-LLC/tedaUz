a61e5efdd235c473467eaf9d29b61543ce92724f
AbdusamadMansurov <107539763+AbdusamadMansurov@users.noreply.github.com>
2022-12-06 17:09:11 +0500

    optimization 1

--- src/main/java/com/example/tedabot/service/SiteService.java
+++ src/main/java/com/example/tedabot/service/SiteService.java
@@ -13,10 +13,7 @@ import com.example.tedabot.repository.ReviewRepository;
 import com.example.tedabot.repository.SiteHistoryRepository;
 import com.example.tedabot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
