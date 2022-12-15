01be5141d0148f5ee5d318d2cd9342bb8819ed88
AbdusamadMansurov <107539763+AbdusamadMansurov@users.noreply.github.com>
2022-12-15 09:39:49 +0500

    order

---
+++ src/main/java/com/example/tedabot/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.example.tedabot.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mansurov Abdusamad  *  14.12.2022  *  12:37   *  tedaUz
+ */
+@RestController
+@RequestMapping("/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+//    @GetMapping()
+//    public Res
+
+}
